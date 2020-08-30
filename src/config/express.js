097db24e669f48ebcb94d95d91581ffbb866d01c
rodrigo <rodsussumu@gmail.com>
2020-08-30 16:18:45 -0300

    Corrige consign

--- src/config/express.js
+++ src/config/express.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-consign({cwd: process.cwd()})
-    .then('/src/config/database.js')
-    .then('/src/app/api')
-    .then('/src/app/routes')
+consign({cwd: 'src'})
+    .include('/app/api')
+    .then('/config/database.js')
+    .then('/app/routes')
     .into(app)
 
 module.exports = app
